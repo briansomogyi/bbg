0641edfb4e110a89561836351fee55653943ea53
briansomogyi <brian.somogyi@emanuel.ro>
2024-03-21 12:15:13 +0200

    Am mai adaugat si pupilele, pe langa ochii unei fete.

--- script.js
+++ script.js
@@ -4,7 +4,17 @@ function setup() {
 
 function draw() {
     background("green");
+    // face
+    fill("white");
+    stroke("black");
     circle(200, 200, 50);
+    // eyes
+    stroke("orange");
     circle(190, 190, 5);
+    stroke("blue");
+    circle(190, 190, 1);
+    stroke("orange");
     circle(210, 190, 5);
+    stroke("blue");
+    circle(210, 190, 1);
 }
