6b2cf6db8ca4741224acb2f46ffdcfde337529cd
etimarita <etian.marita@emanuel.ro>
2024-03-21 12:44:33 +0200

    drawing

--- script.js
+++ script.js
@@ -1,23 +1,51 @@
 function setup() {
-    createCanvas(400, 400);
+    createCanvas(1200, 400);
 }
 
 function draw() {
     background("green");
+    Edraw();
+    Bdraw();
+}
+
+function Bdraw() {
     // face
     fill("white");
     stroke("black");
-    circle(200, 200, 50);
+    circle(600, 200, 50);
     // eyes
     stroke("orange");
-    circle(190, 190, 5);
+    circle(590, 190, 5);
     stroke("blue");
-    circle(190, 190, 1);
+    circle(590, 190, 1);
     stroke("orange");
-    circle(210, 190, 5);
+    circle(610, 190, 5);
     stroke("blue");
-    circle(210, 190, 1);
+    circle(610, 190, 1);
     // mouth
     stroke("red");
-    arc(200, 210, 20, 10, 0, Math.PI);
+    arc(600, 210, 20, 10, 0, Math.PI);
+}
+
+
+function Edraw() {
+    // face
+    fill("yellow");
+    stroke("black");
+    circle(200, 200, 200);
+    // eyes
+    fill("white");
+    stroke("black");
+    circle(150, 150, 50);
+    fill("white");
+    stroke("blue");
+    
+    circle(150, 150, 10);
+    stroke("black");
+    circle(200, 150, 50);
+    stroke("blue");
+    circle(200, 150, 10);
+    // mouth
+    stroke("pink");
+    arc(200, 210, 70, 30, 0, Math.PI);
 }
