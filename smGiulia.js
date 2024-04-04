00585be007b742375166259a4f94eda4f7f78884
giulianaa23 <giulianacristea06@gmail.com>
2024-04-04 12:06:26 +0300

    5

--- smGiulia.js
+++ smGiulia.js
@@ -2,13 +2,13 @@ const smGiulia = {
     x: 1000,
     y: 200,
     direction: 1,
-    speed:3
+    speed: 5,
 }
 function checkLimitsmGiulia() {
     if (smGiulia.x >= tableWidth || smGiulia.x <= 0) {
         smGiulia.direction *= -1;
     }
-    smGiulia.x += smGiulia.direction*smGiulia.speed;
+    smGiulia.x += smGiulia.direction * smGiulia.speed;
 }
 
 function smileyFaceGiulia(x, y) {
