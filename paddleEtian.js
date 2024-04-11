bd038c7b9a35889731e985ab115b19de1aaa1ee1
etimarita <etian.marita@emanuel.ro>
2024-04-11 10:15:10 +0300

    paddleModify

--- paddleEtian.js
+++ paddleEtian.js
@@ -1,10 +1,12 @@
+const paddleEtian = {
+    x: 100,
+    y: 500,
+    color: "orange",
+    width: 120,
+    height: 80,
+}
+
 function createPaddleEtian() {
-    // paddle
-    fill("orange");
-    stroke("black");
-    circle(200, 450, 200);
-    fill("brown");
-    stroke("black");
-    square(175,540, 50);
-    square(175,590, 50);
+    fill(paddleEtian.color);
+    rect(paddleEtian.x, paddleEtian.y, paddleEtian.width, paddleEtian.height);
 }
